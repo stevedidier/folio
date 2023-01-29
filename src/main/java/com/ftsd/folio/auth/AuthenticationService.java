593ce075bd715455da59e81b21bc33a60fb99b7d
stevedidier <didierfotso01@gmail.com>
2023-01-29 07:56:32 +0100

    Release version 0.1.1

--- src/main/java/com/ftsd/folio/auth/AuthenticationService.java
+++ src/main/java/com/ftsd/folio/auth/AuthenticationService.java
@@ -24,22 +24,22 @@ public class AuthenticationService {
   private final JwtService jwtService;
   private final AuthenticationManager authenticationManager;
 
-  public AuthenticationResponse register(RegisterRequest request) {
-    var user = User.builder()
-        .firstname(request.getFirstname())
-        .lastname(request.getLastname())
-        .email(request.getEmail())
-        .password(passwordEncoder.encode(request.getPassword()))
-        .role(Role.USER) 
-        .build();
-    user.setCreatedAt(new Date());
-    user.setUpdatedAt(new Date());
-    repository.save(user);
-    var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder()
-        .token(jwtToken)
-        .build();
-  }
+//   public AuthenticationResponse register(RegisterRequest request) {
+//     var user = User.builder()
+//         .firstname(request.getFirstname())
+//         .lastname(request.getLastname())
+//         .email(request.getEmail())
+//         .password(passwordEncoder.encode(request.getPassword()))
+//         .role(Role.USER) 
+//         .build();
+//     user.setCreatedAt(new Date());
+//     user.setUpdatedAt(new Date());
+//     repository.save(user);
+//     var jwtToken = jwtService.generateToken(user);
+//     return AuthenticationResponse.builder()
+//         .token(jwtToken)
+//         .build();
+//   }
 
   public AuthenticationResponse authenticate(AuthenticationRequest request) {
 
